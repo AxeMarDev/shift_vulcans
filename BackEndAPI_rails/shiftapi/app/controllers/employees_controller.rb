631a9cfd5e5fe722415a6e6639c3c8ec93eb5562
Axell Martinez Avila <axell.martinezavila@gmail.com>
2023-09-25 23:17:40 -0500

    Added employees route
    to view and update employee list

---
+++ BackEndAPI_rails/shiftapi/app/controllers/employees_controller.rb
@@ -0,0 +1,24 @@
+class EmployeesController < ApplicationController
+
+  def index
+    # will 'return' a json displaing all members of employees table
+    render json: Employee.all
+
+  end
+  def create
+    # will create a new employee before saving to table
+    employee = Employee.new( employee_params)
+
+    # will attempt to save to table, if failed, a error will be returned
+    unless employee.save
+      render json: "error", status: 400
+    end
+
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:name,:bio,:clockin)
+  end
+
+end
--- BackEndAPI_rails/shiftapi/config/routes.rb
+++ BackEndAPI_rails/shiftapi/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Will be basic route to get the list of employees;
+  # Will be basic route to add the list of employees
+  resources :employees, only: [:index, :create] # GET && POST
+  # request template
+  # curl --request GET http://localhost:3000/employees
+  # curl --header "Content-Type: application/json" --request POST --data '{"name":"user1", "bio":"worker", "clockin":"false"}' http://localhost:3000/employees
+
+
+
+
+  # shortcuts
+  # rails g controller <controllername> <arguments>
+  # rails g model <modelname> <types>
+
+
 end
