ae4faf57ce45e9f6d3c14ab7be98a8b94390d345
Axell Martinez Avila <axell.martinezavila@gmail.com>
2023-10-02 13:19:59 -0500

    Added functionality to clockin and out any employee in lisgt

--- BackEndAPI_rails/shiftapi/config/routes.rb
+++ BackEndAPI_rails/shiftapi/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   #this will get all the company info and return it client: for admin login
   resources :company, only: [:index, :create]
   # curl --header "Content-Type: application/json"  --request GET --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"axellelectric"}'  http://localhost:3000/company
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   # curl --header "Content-Type: application/json"  --request POST --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"company1", "employeename":"axell","employeepass":"password" }'  http://localhost:3000/companyemployees
   # curl --header "Content-Type: application/json"  --request GET --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"axellelectric" }'  http://localhost:3000/companyemployees
 
-
+  resources :companyemployee, only:[ :update]
 
 
 
