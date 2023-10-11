6ef42abcab986b17c12750911e50344a876a2ff0
Axell Martinez Avila <axell.martinezavila@gmail.com>
2023-10-10 20:27:14 -0500

    Auto stash before merge of "master" and "origin/master"

--- BackEndAPI_rails/shiftapi/config/routes.rb
+++ BackEndAPI_rails/shiftapi/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'employee/index'
 
 
+
   #this will get all the company info and return it client: for admin login
   resources :company, only: [:index, :create]
   # curl --header "Content-Type: application/json"  --request GET --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"axellelectric"}'  http://localhost:3000/company
@@ -12,12 +13,11 @@ Rails.application.routes.draw do
   resources :companyemployees, only: [:index, :create, :destroy]
   # curl --header "Content-Type: application/json"  --request POST --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"company1", "employeename":"axell","employeepass":"password" }'  http://localhost:3000/companyemployees
   # curl --header "Content-Type: application/json"  --request GET --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"axellelectric" }'  http://localhost:3000/companyemployees
-
   post 'authenticate', to: 'authentication#create'
-
   resources :companyemployee, only:[ :update]
   resources :employee, only:[:index]
 
+  #notes
 
 
 
