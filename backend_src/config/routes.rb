56c0dec29580a503ace703e5a02214d3a0033096
Axell Martinez Avila <axell.martinezavila@gmail.com>
2023-10-15 14:14:53 -0500

    Removed routes that aren't actively used

---
+++ backend_src/config/routes.rb
@@ -0,0 +1,35 @@
+Rails.application.routes.draw do
+
+  #this will get all the company info and return it client: for admin login
+  resources :company, only: [:index, :create]
+  # curl --header "Content-Type: application/json"  --request GET --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"axellelectric"}'  http://localhost:3000/company
+  # curl --header "Content-Type: application/json"  --request POST --data '{"adminname":"", "adminpassword":"password", "companyname":"cs"}'  http://localhost:3000/company
+
+  # this will get the list of employees or create an employee
+  resources :companyemployees, only: [:index, :create, :destroy]
+  # curl --header "Content-Type: application/json"  --request POST --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"company1", "employeename":"axell","employeepass":"password" }'  http://localhost:3000/companyemployees
+  # curl --header "Content-Type: application/json"  --request GET --data '{"adminname":"user1", "adminpassword":"2002068", "companyname":"axellelectric" }'  http://localhost:3000/companyemployees
+  post 'authenticate', to: 'authentication#create'
+  resources :companyemployee, only:[ :update]
+  resources :employee, only:[:index]
+
+
+  
+  # ADDING COLUMN TO TABLE
+  # rails g migration AddAgeToEmployees age:integer // will add age to Employees
+  # rails db:migrate
+
+  # mysql
+  # mysql.server start
+  # mysql.server stop
+  # SHOW DATABASES
+  # CREATE DATABASE
+
+  # shortcuts
+  # rails c
+  # rails db:migrate // do not migrate model that is present in database
+  # rails g controller <controllername> <arguments>
+  # rails g model <modelname> <types>
+
+
+end
